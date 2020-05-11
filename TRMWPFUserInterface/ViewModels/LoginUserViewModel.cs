2566475cd96c9528ab60020594b0df39e2eb8fd4
Negin Kordi nejad <neginkrd@gmail.com>
2020-05-11 12:31:50 +1000

    Add Login Form
    add login usercontol

---
+++ TRMWPFUserInterface/ViewModels/LoginUserViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TRMWPFUserInterface.ViewModels
+{
+    public class LoginUserViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+        public bool CanLogIn(string userName, string password)
+        {
+            bool output = false;
+            //   if (userName?.Length > 0 && password?.Length > 0)
+            if (!String.IsNullOrWhiteSpace(userName) && !String.IsNullOrWhiteSpace(password))
+            {
+                output = true;
+            }
+            return output;
+
+        }
+        public void LogIn(string userName, string password)
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
--- TRMWPFUserInterface/ViewModels/ShellViewModel.cs
+++ TRMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace TRMWPFUserInterface.ViewModels
 {
-   public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginUserViewModel _LoginVM;
+        public ShellViewModel(LoginUserViewModel loginVM)
+        {
+            _LoginVM = loginVM;
+            ActivateItem(_LoginVM);
+        }
     }
 }
