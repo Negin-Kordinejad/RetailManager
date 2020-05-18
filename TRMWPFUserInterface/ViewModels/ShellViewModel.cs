dd45bf3b90fd2627edc85c15f82fdfcce4335f84
Negin Kordi nejad <neginkrd@gmail.com>
2020-05-18 17:18:33 +1000

    Edit ShelViewModel DI
    Replace cotainer with IoC

--- TRMWPFUserInterface/ViewModels/ShellViewModel.cs
+++ TRMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -12,14 +12,13 @@ namespace TRMWPFUserInterface.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
-        public ShellViewModel(IEventAggregator events,SalesViewModel salesVM,SimpleContainer container)
+       
+        public ShellViewModel(IEventAggregator events,SalesViewModel salesVM)
         { 
             _events = events;
             _salesVM = salesVM;
-            _container = container;
             _events.Subscribe(this);
-            ActivateItem(_container.GetInstance<LoginUserViewModel>());
+            ActivateItem(IoC.Get<LoginUserViewModel>());
            
         }
 
